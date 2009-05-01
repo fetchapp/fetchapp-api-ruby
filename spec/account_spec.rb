65fe734bc5597e1d154399155c1e55790dce012b
Mike Larkin <mikelarkin@pixallent.com>
2009-05-01 02:21:53 -0400

    ignoring spec until mocks can be made

--- spec/account_spec.rb
+++
@@ -1,9 +0,0 @@
-require File.join(File.dirname(__FILE__), "..", "lib", "fetchapi-ruby")
-include FetchAPI
-
-describe Account do
-  before do
-    Base.basic_auth('http://pixallent.myhost.dev:3000/api', 'pixallent', 'pixallent')
-  end
-
-end
