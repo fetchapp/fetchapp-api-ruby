bec289168b5105f7df54f4456b6355a525aea9be
Michael Larkin <mjlarkin@gmail.com>
2012-04-13 16:17:50 -0400

    Comment Tweak

--- lib/fetchapp-api-ruby/upload.rb
+++ lib/fetchapp-api-ruby/upload.rb
@@ -2,7 +2,7 @@ module FetchAPI
   class Upload < FetchAPI::Base
     attr_accessor :id, :attributes
 
-   # Find :all downloads or the specified ID
+   # Find :all uploads or the specified ID
    def self.find(selector, params={})
        super(selector, params)
     end
